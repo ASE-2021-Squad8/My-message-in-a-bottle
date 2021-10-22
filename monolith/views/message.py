6c4bd59821498f53e0910a3771dcf70120d3c0e4
0xfederama <vgfede@gmail.com>
2021-10-22 17:01:06 +0200

    displayed firstname and lastname in dropdown, fixed bug in message.py line 11

--- monolith/views/message.py
+++ monolith/views/message.py
@@ -8,6 +8,5 @@ message = Blueprint('message', __name__)
 @message.route('/send_message')
 def _send():
     # Only displaying recipients list for now
-    _users = db.session
+    _users = db.session.query(User)
     return render_template("send_message.html", users=_users)
-    
