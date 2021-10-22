d59c4bb42f1d26d4fd448943656367f3bcf3ced4
0xfederama <vgfede@gmail.com>
2021-10-22 16:37:40 +0200

    Add drop-down view for recipients list and basic send_message.html

---
+++ monolith/views/message.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, render_template
+
+from monolith.database import User, db
+
+message = Blueprint('message', __name__)
+
+
+@message.route('/send_message')
+def _send():
+    # Only displaying recipients list for now
+    _users = db.session
+    return render_template("send_message.html", users=_users)
+    
