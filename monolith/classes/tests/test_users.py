eb3a27b5c2fd6a117d70b77b4295f48441c92e64
Kostantino <kostantinoprifti9@gmail.com>
2021-10-26 14:48:08 +0200

    Test for update user data and change password done!

--- monolith/classes/tests/test_users.py
+++ monolith/classes/tests/test_users.py
@@ -3,6 +3,7 @@ import unittest
 from monolith.app import create_test_app
 from monolith.database import User, db
 from monolith.auth import current_user
+import datetime
 
 
 class TestApp(unittest.TestCase):
@@ -47,3 +48,88 @@ class TestApp(unittest.TestCase):
         # Check the user in the db has is_active=False
         user = User.query.filter(User.email == "test@test.com").first()
         assert not user.get_isactive()
+
+    def test_not_authenticated_update_data(self):
+        reply = self.client.get("/update_data")
+        assert reply.status_code == 401
+
+    def test_update_data(self):
+        reply = self.client.post(
+            "/create_user",
+            data=dict(
+                email="test_update@test.com",
+                firstname="test",
+                lastname="test",
+                password="test",
+                dateofbirth="1/1/1111",
+            ),
+            follow_redirects=True,
+        )
+        assert reply.status_code == 200
+
+        reply = self.client.post(
+            "/login",
+            data=dict(
+                email="test_update@test.com",
+                password="test",
+            ),
+            follow_redirects=True,
+        )
+        assert reply.status_code == 200
+        reply = self.client.post(
+            "/update_data",
+            data=dict(
+                textfirstname="test_new",
+                textlastname="test_new",
+                textbirth="2021-02-02",
+                textemail="test_new@test.com",
+            ),
+            follow_redirects=True,
+        )
+        assert reply.status_code == 200
+        user = User.query.filter(User.email == "test_new@test.com").first()
+        assert user is not None
+        assert user.firstname == "test_new"
+        assert user.lastname == "test_new"
+        assert user.dateofbirth == datetime.datetime(2021, 2, 2)
+
+    def test_not_authenticated_update_password(self):
+        reply = self.client.get("/reset_password")
+        assert reply.status_code == 401
+
+    def test_update_password(self):
+        reply = self.client.post(
+            "/create_user",
+            data=dict(
+                email="pass_update@test.com",
+                firstname="test",
+                lastname="test",
+                password="test",
+                dateofbirth="1/1/1111",
+            ),
+            follow_redirects=True,
+        )
+        assert reply.status_code == 200
+
+        reply = self.client.post(
+            "/login",
+            data=dict(
+                email="pass_update@test.com",
+                password="test",
+            ),
+            follow_redirects=True,
+        )
+        assert reply.status_code == 200
+        reply = self.client.post(
+            "/reset_password",
+            data=dict(
+                currentpassword="test",
+                newpassword="test_new",
+                confirmpassword="test_new",
+            ),
+            follow_redirects=True,
+        )
+        assert reply.status_code == 200
+        user = User.query.filter(User.email == "pass_update@test.com").first()
+        assert user is not None
+        assert user.authenticate("test_new")
--- monolith/views/users.py
+++ monolith/views/users.py
@@ -1,4 +1,3 @@
-
 from flask import Blueprint, redirect, render_template, request, jsonify
 from flask_login import logout_user
 
@@ -8,6 +7,7 @@ from monolith.forms import UserForm, ChangePassForm
 from monolith.auth import check_authenticated, current_user
 import monolith.user_query
 import datetime
+
 users = Blueprint("users", __name__)
 
 
@@ -22,7 +22,7 @@ def _user():
     check_authenticated()
     data = current_user.dateofbirth
     date_of_birth = data.strftime("%a, %d %B, %Y")
-    return render_template("account_data.html", user=current_user, date = date_of_birth)
+    return render_template("account_data.html", user=current_user, date=date_of_birth)
 
 
 @users.route("/create_user", methods=["POST", "GET"])
@@ -50,7 +50,6 @@ def create_user():
         raise RuntimeError("This should not happen!")
 
 
-
 @users.route("/user/get_recipients", methods=["GET"])
 def get_recipients():
     result = monolith.user_query.get_recipients(getattr(current_user, "id"))
@@ -58,6 +57,7 @@ def get_recipients():
     for usr in result:
         l.append(usr.as_dict())
     return jsonify(l)
+
 
 @users.route("/unregister")
 def unregister():
@@ -69,39 +69,51 @@ def unregister():
     db.session.commit()
     return redirect("/")
 
-@users.route('/update_data', methods=['POST', 'GET'])
+
+@users.route("/update_data", methods=["POST", "GET"])
 def change_data_user():
     check_authenticated()
     form = UserForm()
-    
-    if request.method == 'POST':
+
+    if request.method == "POST":
         userid = getattr(current_user, "id")
         user = User.query.filter(User.id == userid).first()
-        user.firstname =request.form['textfirstname']
-        user.lastname = request.form['textlastname']
-        user.email = request.form['textemail']
-        data = request.form['textbirth']
-        date_as_datetime = datetime.datetime.strptime(data, '%Y-%m-%d')
+        user.firstname = request.form["textfirstname"]
+        user.lastname = request.form["textlastname"]
+        user.email = request.form["textemail"]
+        data = request.form["textbirth"]
+        date_as_datetime = datetime.datetime.strptime(data, "%Y-%m-%d")
         user.dateofbirth = date_as_datetime
-        #update_password = request.form['textpass']
-        
-        if(user.firstname == "" or user.lastname=="" or user.email=="" or user.dateofbirth == ""):
-            return render_template('edit_profile.html', form=form, user=current_user, error = "All fields must be completed!")
-        
+        # update_password = request.form['textpass']
+
+        if (
+            user.firstname == ""
+            or user.lastname == ""
+            or user.email == ""
+            or user.dateofbirth == ""
+        ):
+            return render_template(
+                "edit_profile.html",
+                form=form,
+                user=current_user,
+                error="All fields must be completed!",
+            )
+
         db.session.commit()
         return redirect("/account_data")
-    elif request.method == 'GET':
-        return render_template('edit_profile.html', form=form, user=current_user)
+    elif request.method == "GET":
+        return render_template("edit_profile.html", form=form, user=current_user)
     else:
-        raise RuntimeError('This should not happen!')
+        raise RuntimeError("This should not happen!")
 
-@users.route('/reset_password', methods=['POST', 'GET'])
+
+@users.route("/reset_password", methods=["POST", "GET"])
 def change_pass_user():
     check_authenticated()
     form = ChangePassForm()
     stringError = ""
 
-    if request.method == 'POST':
+    if request.method == "POST":
         userid = getattr(current_user, "id")
         user = User.query.filter(User.id == userid).first()
         current_password = form.currentpassword.data
@@ -113,15 +125,19 @@ def change_pass_user():
                 user.set_password(new_pass)
             else:
                 stringError = "Check the correctness of the confirmation password!"
-                return render_template('reset_password.html', form=form, error = stringError)
+                return render_template(
+                    "reset_password.html", form=form, error=stringError
+                )
         else:
             stringError = "Wrong current password!"
-            return render_template('reset_password.html', form=form, error = stringError)
+            return render_template("reset_password.html", form=form, error=stringError)
 
         db.session.commit()
-        return render_template('reset_password.html',form=form, success = "Password updated!")
+        return render_template(
+            "reset_password.html", form=form, success="Password updated!"
+        )
 
-    elif request.method == 'GET':
-        return render_template('reset_password.html', form = form)
+    elif request.method == "GET":
+        return render_template("reset_password.html", form=form)
     else:
-        raise RuntimeError('This should not happen!')
+        raise RuntimeError("This should not happen!")
