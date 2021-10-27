d26827b675b05c8635c96c14c779570f7058d9ac
Eli M <eli@meluc.ci>
2021-10-27 23:19:03 +0200

    message: replace outdated endpoints

--- monolith/classes/tests/test_messages.py
+++ monolith/classes/tests/test_messages.py
@@ -1,7 +1,8 @@
-import json
 import unittest
 
+from flask import json, jsonify
 from monolith.app import create_test_app
+
 
 class TestApp(unittest.TestCase):
     def setUp(self):
@@ -11,45 +12,48 @@ class TestApp(unittest.TestCase):
         self._ctx.push()
 
     def test_unauthorised_access(self):
-        reply = self.client.post('/api/message/draft', data=dict(text=""))
+        reply = self.client.post("/api/message/draft", data=dict(text=""))
         assert reply.status_code == 401
 
-    def test_empty_draft(self): 
+    def test_empty_draft(self):
         with self.client:
-            reply = self.client.post('/login', data=dict(
-                email="example@example.com",
-                password="admin"
-            ), follow_redirects=True)
+            reply = self.client.post(
+                "/login",
+                data=dict(email="example@example.com", password="admin"),
+                follow_redirects=True,
+            )
 
-            reply = self.client.post('/api/message/draft', data=dict(text=""))
+            reply = self.client.post("/api/message/draft", data=dict(text=""))
             assert reply.status_code == 400
-
 
     def test_draft_insert_delete(self):
         with self.client:
-            reply = self.client.post('/login', data=dict(
-                email="example@example.com",
-                password="admin"
-            ), follow_redirects=True)
+            reply = self.client.post(
+                "/login",
+                data=dict(email="example@example.com", password="admin"),
+                follow_redirects=True,
+            )
 
-            reply = self.client.get('/api/message/draft/all')
+            reply = self.client.get("/api/message/draft/all")
             assert reply.status_code == 200
 
-            reply = self.client.post('/api/message/draft', data=dict(text="Lorem ipsum dolor..."))
+            reply = self.client.post(
+                "/api/message/draft", data=dict(text="Lorem ipsum dolor...")
+            )
             data = reply.get_json()
             assert reply.status_code == 200
-            assert data['message_id'] == 1
+            assert data["message_id"] == 1
 
-            reply = self.client.get('/api/message/user_drafts')
+            reply = self.client.get("/api/message/draft/all")
             data = reply.get_json()
             assert reply.status_code == 200
-            assert data[0]['text'] == "Lorem ipsum dolor..."
-        
-            reply = self.client.delete('/api/message/draft', data=dict(message_id=1))
+            assert data[0]["text"] == "Lorem ipsum dolor..."
+
+            reply = self.client.delete("/api/message/draft/1")
             data = reply.get_json()
             assert reply.status_code == 200
-            assert int(data['message_id']) == 1
+            assert int(data["message_id"]) == 1
 
-            reply = self.client.delete('/api/message/draft', data=dict(message_id=1))
+            reply = self.client.delete("/api/message/draft/1")
             data = reply.get_json()
             assert reply.status_code == 404
