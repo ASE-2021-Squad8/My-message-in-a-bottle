a30537898839d0d2fe50fdd956dcd56c36b29de0
Eli M <eli@meluc.ci>
2021-11-02 14:06:47 +0100

    user_query: rename blacklist symbols, document functions

--- monolith/user_query.py
+++ monolith/user_query.py
@@ -1,57 +1,88 @@
-from monolith.database import User, db, Black_list
+from monolith.database import User, db, BlackList
 
 
 def get_recipients(sender_id):
+    """Gets the list of possible recipients for a specific user,
+    accounting for the blasklist
+
+    :param sender_id: user id of the sender
+    :type sender_id: int
+    :return: resulting database query
+    :rtype: Query
+    """
+
     result = (
         db.session.query(User.id, User.email)
         .filter(User.id != sender_id)
         .filter(User.is_active)
         .filter(
             User.id.not_in(
-                db.session.query(Black_list.member).filter(
-                    Black_list.owner == sender_id
-                )
+                db.session.query(BlackList.member).filter(BlackList.owner == sender_id)
             )
         )
     )
     return result
 
 
-def get_black_list(owner_id):
-    result_ = []
+def get_blacklist(owner_id):
+    """Retrieves the blacklist owned by a specific user
+
+    :param owner_id: user id for the blacklist owner
+    :type owner_id: int
+    :return: a list of blocked users
+    :rtype: list[User]
+    """
+
+    result = []
     try:
-        result_ = (
-            db.session.query(Black_list.member, User.email)
-            .filter(Black_list.owner == owner_id)
-            .filter(Black_list.member == User.id)
+        result = (
+            db.session.query(BlackList.member, User.email)
+            .filter(BlackList.owner == owner_id)
+            .filter(BlackList.member == User.id)
             .all()
         )
     except Exception as e:
         print("Exception in get_black_list %r", e)
 
-    return result_
+    return result
 
 
-def add_users_to_black_list(owner_id, members_id):
-    _result = False
+def add_to_blacklist(owner_id, members_id):
+    """Adds a new user to a blacklist
+
+    :param owner_id: the user id of the blacklist owner
+    :type owner_id: int
+    :param members_id: the user id to add to the blacklist
+    :type members_id: int
+    :return: True if the user was added to the blacklist, False otherwise
+    :rtype: bool
+    """
+
+    result = False
     try:
         for member_id in members_id:
-            entry = Black_list()
-            entry.owner = owner_id
-            entry.member = member_id
+            entry = BlackList(owner=owner_id, member=member_id)
             db.session.add(entry)
 
         db.session.commit()
-        _result = True
+        result = True
     except Exception as e:
         db.session.rollback()
         print("Exception in add_user_to_black_list %r", e)
 
-    return _result
+    return result
 
 
-def get_choices(owner_id):
-    _result = []
+def get_blacklist_candidates(owner_id):
+    """Computes a list of users that can be added to a blacklist, depending on the owner
+
+    :param owner_id: the id for the user that owns the blacklist
+    :type owner_id: int
+    :return: list of blacklist-able users
+    :rtype: list[User]
+    """
+
+    result = []
     try:
         result = (
             db.session.query(User.id, User.email)
@@ -59,34 +90,45 @@ def get_choices(owner_id):
             .filter(User.is_admin == 0)
             .filter(
                 User.id.not_in(
-                    db.session.query(Black_list.member).filter(
-                        Black_list.owner == owner_id
+                    db.session.query(BlackList.member).filter(
+                        BlackList.owner == owner_id
                     )
                 )
             )
             .all()
         )
-        _result = [(usr.id, usr.email) for usr in result]
+        result = [(usr.id, usr.email) for usr in result]
     except Exception as e:
         print("Exception in get_choices %r", e)
-    return _result
+
+    return result
 
 
-def delete_users_black_list(owner_id, members_id):
-    _result = False
+def remove_from_blacklist(owner_id, members_id):
+    """Removes a user from a blacklist
+
+    :param owner_id: the user id of the blacklist owner
+    :type owner_id: int
+    :param members_id: the user id to remove from the blacklist
+    :type members_id: int
+    :return: True if the user was removed from the blacklist, False otherwise
+    :rtype: bool
+    """
+
+    result = False
     try:
         for member_id in members_id:
-            db.session.query(Black_list).filter(Black_list.owner == owner_id).filter(
-                Black_list.member == member_id
+            db.session.query(BlackList).filter(BlackList.owner == owner_id).filter(
+                BlackList.member == member_id
             ).delete()
 
         db.session.commit()
-        _result = True
+        result = True
     except Exception as e:
         db.session.rollback()
         print("Exception in delete_user_black_list %r", e)
 
-    return _result
+    return result
 
 
 def get_user_mail(user_id):
