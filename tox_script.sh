0a799b2de1c36a2d04ac8d6d348d5b0624775486
carloleo <carlo_leo@outlook.it>
2021-11-02 22:36:02 +0100

    commeted coveralls command

--- tox_script.sh
+++ tox_script.sh
@@ -2,4 +2,4 @@ python3 -m smtpd -c DebuggingServer -n localhost:1025 &
 celery -A monolith.background  worker -l INFO -Q message --detach #for test send message by celery
 rm -rf mmiab-test.db
 pytest --cov-config .coveragerc --cov monolith monolith/classes/tests -s
-coveralls
+#coveralls
