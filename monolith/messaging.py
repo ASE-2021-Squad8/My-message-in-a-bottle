4332a41931c9e1ccaea1a8acf2d7a8fbfdc885de
Kostantino <kostantinoprifti9@gmail.com>
2021-11-04 14:15:23 +0100

    calendar implementation completed

--- monolith/messaging.py
+++ monolith/messaging.py
@@ -2,6 +2,8 @@ import json
 import os
 from datetime import datetime
 from re import M
+from monolith.auth import check_authenticated
+from monolith.database import Message, db, User
 import json
 from monolith.notifications import send_notification
 from monolith.user_query import get_user_mail
@@ -203,3 +205,24 @@ def check_message_to_send():
 
     db.session.commit()
     return ids
+def get_day_message(userid, baseDate, upperDate):
+    check_authenticated()
+    q= db.session.query(Message).filter(Message.sender == userid, Message.delivery_date >= baseDate, Message.delivery_date < upperDate)
+    
+    list = []
+
+    for msg in q:
+        
+        recipient = (
+            db.session.query(User).filter(User.id == msg.get_recipient()).first()
+        )
+
+        json_msg = json.dumps(
+            {
+                "firstname": recipient.get_firstname(),
+                "text": msg.text,
+            }
+        )
+
+        list.append(json_msg)
+    return list
--- monolith/views/message.py
+++ monolith/views/message.py
@@ -350,3 +350,16 @@ def read_msg(id):
         )
 
     return jsonify({"msg_read": True})
+    
+@msg.route("/api/calendar")
+def calendar():
+    return render_template("calendar.html")
+
+@msg.route("/api/calendar/<int:day>/<int:month>/<int:year>")
+def calendar_send_message(day, month, year):
+    basedate = datetime(year,month +1, day)
+    upperdate = datetime(year,month +1, day + 1)
+    userid = getattr(current_user, "id")
+    message = monolith.messaging.get_day_message(userid, basedate, upperdate)
+    return jsonify(message)
+
