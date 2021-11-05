9e20e1ddfddabb3d58193109ee4d74612687854c
E. Melucci <eli@meluc.ci>
2021-11-05 19:55:43 +0100

    notifications: use environment variables to manage mailserver password
    Signed-off-by: E. Melucci <eli@meluc.ci>

--- monolith/notifications.py
+++ monolith/notifications.py
@@ -2,30 +2,26 @@ import smtplib
 from email.message import EmailMessage
 import os
 
-smtp_server = (
-    "smtp.gmail.com"
-    if not "MAIL_SERVER" in os.environ
-    else os.environ.get("MAIL_SERVER")
+PORT = int(os.environ.get("MAIL_PORT", 587))
+NOTIFICATIONS_EMAIL = os.environ.get(
+    "MAIL_NOREPLY_ADDRESS", "mmiab.notifications@gmail.com"
 )
-port = 587 if not "MAIL_PORT" in os.environ else int(os.environ.get("MAIL_PORT"))
-notifications_email = "mmiab.notifications@gmail.com"
+SMTP_SERVER = os.environ.get("MAIL_SERVER", "smtp.gmail.com")
+SMTP_PASSWORD = os.environ.get("MAIL_SERVER_PASSWORD", "")
 
 
 def send_notification(msg_sender, receiver, msg_body):
-
     # Log in to server and send email
     server = None
     try:
-        with open("password.txt", "r") as pwfile:
-            password = pwfile.readline()
-            pwfile.close()
-        server = smtplib.SMTP(smtp_server, port)
-        server.starttls()
-        server.login(notifications_email, password)
+        server = smtplib.SMTP(SMTP_SERVER, PORT)
+        if SMTP_PASSWORD != "":
+            server.starttls()
+            server.login(NOTIFICATIONS_EMAIL, SMTP_PASSWORD)
         mail = EmailMessage()
         mail["Subject"] = "MMIAB - Message from " + msg_sender
         mail.set_content(msg_body)
-        server.sendmail(notifications_email, receiver, mail.as_string())
+        server.sendmail(NOTIFICATIONS_EMAIL, receiver, mail.as_string())
     except Exception as e:
         # Print any error messages
         print(e)
