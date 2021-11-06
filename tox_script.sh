ebe08cd6dca9a050acd5d80205a8602d067265e8
Ely M <sykhro@users.noreply.github.com>
2021-11-06 10:36:35 +0100

    ci: make pytest verbose

--- tox_script.sh
+++ tox_script.sh
@@ -1,5 +1,5 @@
 python3 -m smtpd -c DebuggingServer -n localhost:1025 &
 celery -A monolith.background  worker -l INFO -Q message --detach #for test send message by celery
 rm -rf mmiab-test.db
-pytest --cov-config .coveragerc --cov monolith monolith/classes/tests -s
+pytest --cov-config .coveragerc --cov monolith monolith/classes/tests -s -v
 #coveralls
