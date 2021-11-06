380ef169e21099da8a9c3654d8e516f5a7c6a023
E. Melucci <eli@meluc.ci>
2021-11-06 15:16:45 +0100

    fix wrong parameter number in run.sh
    Signed-off-by: E. Melucci <eli@meluc.ci>

--- run.sh
+++ run.sh
@@ -17,6 +17,7 @@ then
     export MAIL_NOREPLY_ADDRESS=$4
     export MAIL_SERVER_PASSWORD=$5
 fi
+
 celery -A monolith.background worker -l INFO -Q message --detach # for sending messages 
 celery -A monolith.background worker -l INFO -Q notification --detach # for sending email
 celery -A monolith.background worker -l INFO --detach # for periodic tasks
@@ -25,7 +26,7 @@ celery -A monolith.background beat -l INFO --detach # for scheduling period task
 if [[ $1 == "DEV" ]]
 then
     flask run
-elif [[ $2 == "PROD" ]]
+elif [[ $1 == "PROD" ]]
 then
     waitress-serve --call 'monolith:create_app'
 fi
