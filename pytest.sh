7c5f7490419ca43a2d1d5c92e96b10487595095b
Federico <vgfede@gmail.com>
2021-11-09 18:33:39 +0100

    add pytest.sh

---
+++ pytest.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+pip install pytest pytest-cov
+
+python3 -m smtpd -c DebuggingServer -n localhost:1025 &
+celery -A monolith.background worker -l INFO -Q message --detach
+rm -rf mmiab-test.db
+pytest -s -v --cov monolith monolith/classes/tests --cov-report term-missing
